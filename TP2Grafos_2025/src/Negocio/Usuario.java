02f3cd7219ccf2836651123c65b6806ebfabc275
leansan93 <sanchezleandro988@gmail.com>
2025-09-26 19:49:00 -0300

    primer commit en el que estan los paquetes para la interface (todavia no agregue botones ni nada ) y otro para la parte de negocio en el que esta el Usuario y tiene para calcular la similaridad y que esto se usara para el arco cuando se cree el grafo

---
+++ TP2Grafos_2025/src/Negocio/Usuario.java
@@ -0,0 +1,69 @@
+package Negocio;
+
+public class Usuario {
+	private String nombre;
+	private Integer Interes_tango;
+	private Integer Interes_rockNacional;
+	private Integer Interes_Folclore;
+	private Integer Interes_Urbano;
+	
+	
+	public Usuario( String nombre , Integer  Interes_tango , Integer Interes_rockNacional, Integer Interes_Folclore , Integer Interes_Urbano) {
+		this.nombre= nombre;
+		this.Interes_rockNacional = Interes_rockNacional;
+		this.Interes_Folclore= Interes_Folclore;
+		this.Interes_tango= Interes_tango;
+		this.Interes_Urbano= Interes_Urbano;
+		
+		
+	}
+
+	public int CalcularSimilaridad(Usuario Usuariosegundo) {
+		return Math.abs(Interes_rockNacional - Usuariosegundo.getInteres_rockNacional()) + 
+			   Math.abs(Interes_Folclore - Usuariosegundo.getInteres_Folclore())+
+			   Math.abs(Interes_tango - Usuariosegundo.getInteres_tango()) + 
+			   Math.abs(Interes_Urbano - Usuariosegundo.getInteres_Urbano());
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getInteres_tango() {
+		return Interes_tango;
+	}
+
+	public void setInteres_tango(Integer interes_tango) {
+		Interes_tango = interes_tango;
+	}
+
+	public Integer getInteres_rockNacional() {
+		return Interes_rockNacional;
+	}
+
+	public void setInteres_rockNacional(Integer interes_rockNacional) {
+		Interes_rockNacional = interes_rockNacional;
+	}
+
+	public Integer getInteres_Folclore() {
+		return Interes_Folclore;
+	}
+
+	public void setInteres_Folclore(Integer interes_Folclore) {
+		Interes_Folclore = interes_Folclore;
+	}
+
+	public Integer getInteres_Urbano() {
+		return Interes_Urbano;
+	}
+
+	public void setInteres_Urbano(Integer interes_Urbano) {
+		Interes_Urbano = interes_Urbano;
+	}
+
+
+}
