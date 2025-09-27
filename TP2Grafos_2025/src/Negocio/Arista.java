4e97c12ce940753c08bbf1fabded0f7bb2847ad1
leansan93 <sanchezleandro988@gmail.com>
2025-09-27 02:24:37 -0300

    en la clase grafo se puede generar arista con su peso luego la lista de aristas sera manejado por la clase kruscal para que realice el arbol generador minimo

---
+++ TP2Grafos_2025/src/Negocio/Arista.java
@@ -0,0 +1,50 @@
+package Negocio;
+
+public class Arista  implements Comparable<Arista> {
+	private Integer peso;
+	private Usuario u1;
+	private Usuario u2;
+	
+	public Arista ( Usuario u1 , Usuario u2 ,Integer peso ) {
+		this.peso= peso;
+		this.u1 = u1;
+		this.u2= u2;
+		
+	}
+	
+
+
+	public Integer getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(Integer peso) {
+		this.peso = peso;
+	}
+
+	public Usuario getU1() {
+		return u1;
+	}
+
+
+	public void setU1(Usuario u1) {
+		this.u1 = u1;
+	}
+
+	public Usuario getU2() {
+		return u2;
+	}
+	
+	
+	public void setU2(Usuario u2) {
+		this.u2 = u2;
+	}
+
+////// compara los pesos entre aristas 
+	@Override
+	public int compareTo(Arista otro) {
+		return Integer.compare(peso , otro.peso);
+	}
+
+	
+}
--- TP2Grafos_2025/src/Negocio/Grafo.java
+++ TP2Grafos_2025/src/Negocio/Grafo.java
@@ -5,30 +5,57 @@ import java.util.List;
 
 public class Grafo {
 	
-	List <Usuario> Usuario;
-	List <Aristas> Aristas;
+	List <Usuario> _Usuarios;
+	List <Arista> _Aristas;
 	
 	public Grafo () {
 		
-		this.Usuario= new ArrayList<>();
-		this.Aristas= new ArrayList<>();
+		this._Usuarios= new ArrayList<>();
+		this._Aristas= new ArrayList<>();
 	}
 	
 	
 	public void agregarUsuario(Usuario usuario) {
 		
-		 if (Esvalido(usuario)) {
-			 
-			 Usuario.add(usuario);
-			 
-		 }
+		_Usuarios.add(usuario);
 		
 	}
 
-
-	private boolean Esvalido(Negocio.Usuario usuario2) {
-		 
-		return false;
+	public List<Usuario> getUsuario(){
+		return _Usuarios;
 	}
 
+
+	public List<Arista> getAristas() {
+		return _Aristas;
+	}
+
+
+	public void setAristas(List<Arista> aristas) {
+		_Aristas = aristas;
+	}
+
+
+	public void setUsuario(List<Usuario> usuario) {
+		_Usuarios= usuario;
+	}
+	
+	// generar aristas con peso todos los pares de usuarios con su peso 
+	public void generarAristas() {
+
+		for (int i = 0 ; i < _Usuarios.size() ; i ++ ) {
+			for (int j = i + 1 ; j < _Usuarios.size() ; j ++ ) {
+				Usuario u1= _Usuarios.get(i);
+				Usuario u2 = _Usuarios.get(j);
+				
+				int peso = u1.CalcularSimilaridad(u2);
+				
+				_Aristas.add(new Arista(u1, u2 , peso));
+			}
+			
+		}
+		
+		
+	}
+	 
 }
