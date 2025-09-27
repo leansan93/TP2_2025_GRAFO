b00fa0cc7eb658e53bba0bc9df6b5c63ee4ad182
leansan93 <sanchezleandro988@gmail.com>
2025-09-26 22:36:13 -0300

    agregando el tema de los grafos  contendra listas de los usuarios con listas de aristas que tendra el peso o arco del usuario

--- TP2Grafos_2025/src/Negocio/Grafo.java
+++ TP2Grafos_2025/src/Negocio/Grafo.java
@@ -1,5 +1,34 @@
 package Negocio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grafo {
+	
+	List <Usuario> Usuario;
+	List <Aristas> Aristas;
+	
+	public Grafo () {
+		
+		this.Usuario= new ArrayList<>();
+		this.Aristas= new ArrayList<>();
+	}
+	
+	
+	public void agregarUsuario(Usuario usuario) {
+		
+		 if (Esvalido(usuario)) {
+			 
+			 Usuario.add(usuario);
+			 
+		 }
+		
+	}
+
+
+	private boolean Esvalido(Negocio.Usuario usuario2) {
+		 
+		return false;
+	}
 
 }
