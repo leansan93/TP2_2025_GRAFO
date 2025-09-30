75303ccad22fcdedfbcdae839a576645e2af23e9
leansan93 <sanchezleandro988@gmail.com>
2025-09-30 16:46:15 -0300

    VentanaGrupos aqui es la ventana donde se mostrara los grupos al clickear confirmar

--- TP2Grafos_2025/src/Interface/VentanaGrupos.java
+++ TP2Grafos_2025/src/Interface/VentanaGrupos.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTable;
+import java.awt.Panel;
+import javax.swing.JTabbedPane;
 
 public class VentanaGrupos extends JFrame {
 
@@ -36,6 +41,11 @@ public class VentanaGrupos extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblGrupos = new JLabel("Grupos ");
+		lblGrupos.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblGrupos.setBounds(157, 11, 122, 31);
+		contentPane.add(lblGrupos);
 	}
-
 }
